d69516c038468aed60e29096c0c3f4d020be49d7
Jazib3 <jazib.mahboob@sky.uk>
2017-07-23 18:52:50 +0100

    initial commit

---
+++ Assets/Scripts/ModelModification.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelModification : MonoBehaviour {
+
+	private GameObject dummyModels;
+	private int counter;
+
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+		dummyModels = GameObject.Find("Dummy Models");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void changeCounter(int direction) {
+		// identify direction (left or right)
+		if (direction > 0) {
+			counter++;
+		}
+		else if (direction < 0) {
+			counter--;
+		}
+
+		counter = sanitiseCounter(counter);
+		updateDisplayedModel();
+	}
+
+	public void changeCounterByInt(int newCount) {
+		counter = sanitiseCounter(newCount);
+		updateDisplayedModel();
+	}
+
+	private void updateDisplayedModel() {
+		// remove all current models as a child of this gameobject
+		foreach(Transform child in transform) {
+			Destroy(child.gameObject);
+		}
+
+		// assign the correct model onto this gameobject
+		Transform model = dummyModels.transform.GetChild(counter);
+		Vector3 defaultPos = new Vector3(0, 0, 0);
+		Quaternion defaultRot = new Quaternion(0, 0, 0, 0);
+		Transform clone = Instantiate(model, transform, false);
+		clone.gameObject.SetActive(true);
+	}
+
+	private int sanitiseCounter(int count) {
+		// sanitise for negative values
+		int childrenCount = dummyModels.transform.childCount;
+		if (count < 0) {
+			count = 0;
+		}
+		// sanitise for positive values
+		else if (count >= childrenCount) {
+			count = childrenCount - 1;
+		}
+
+		return count;
+	}
+}
---
+++ Assets/SliderHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderHandler : MonoBehaviour {
+
+	public GameObject target;
+	private Dropdown dd;
+	
+	// Use this for initialization
+	void Start () {
+		// when the dropdown changes, change the model viewed
+		dd = gameObject.GetComponent<Dropdown>();
+		dd.onValueChanged.AddListener(delegate {
+			ValueChange();
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void ValueChange() {
+		int modelCount = dd.value;
+		target.GetComponent<ModelModification>().changeCounterByInt(modelCount);
+	}
+}
